72dbb06d104fa2e9758276aae0e9cf9cf060edc6
Ayush Srivastava <a28863442@gmail.com>
2026-04-03 07:13:41 +0530

    Update P8.c

--- P8.c
+++ P8.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-
-int main(void)
+int main()
 {
- 
+    int marks1, marks2, marks3, marks4, marks5, marks6;
+    printf("Enter marks of subject 1:\n");
+    scanf("%d", &marks1);
+    printf("Enter marks of subject 2:\n");
+    scanf("%d", &marks2);
+    printf("Enter marks of subject 3:\n");
+    scanf("%d", &marks3);
+    printf("Enter marks of subject 4:\n");
+    scanf("%d", &marks4);
+    printf("Enter marks of subject 5:\n");
+    scanf("%d", &marks5);
+    printf("Enter marks of subject 6:\n");
+    scanf("%d", &marks6);
 
+    if (marks1 < 33 || marks2 < 33 || marks3 < 33 ||marks4 < 33 || marks5 < 33 || marks6 < 33)
+    {
+        printf("You are failed due to less marks in individual subject(s)\n");
+    }
+    else if((marks1 + marks2 + marks3 + marks4 + marks5 + marks6) / 6 < 33)
+    {
+        printf("You are failed due to less percentage\n");
+    }
+    else
+    {
+     printf("You are passed\n");
+     }
     return 0;
 }
+
